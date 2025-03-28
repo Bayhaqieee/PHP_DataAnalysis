d1083439db4e9e47f944e95fb2d51539372957e6
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2025-03-28 16:12:42 +0700

    Aggregate

--- aggregate_sql.sql
+++ aggregate_sql.sql
@@ -1,2 +1,8 @@
 -- Menampilkan total nilai semester 1 dan semester 2 dari tabel students
-SELECT SUM(Semester1) as Total_1, SUM(Semester2) as Total_2 FROM students; 
+SELECT SUM(Semester1) as Total_1, SUM(Semester2) as Total_2 FROM students;
+
+-- Menampilkan total nama depan dari tabel students
+SELECT COUNT(FirstName) as Total_Student FROM students;
+
+-- Menampilkan rata-rata nilai semester 1 dan semester 2 dari tabel students
+SELECT AVG(Semester1) as AVG_1, AVG(Semester2) as AVG_2 FROM students;
--- practice_fundamental.sql
+++ practice_fundamental.sql
@@ -1,2 +1,5 @@
 -- Write a SQL query to display the first name using UPPER and last name using LOWER of all students in upper case.
 select StudentID, UPPER(FirstName) as FirstName, LOWER(LastName) as LastName from students;
+
+-- Write a SQL query to display the MIN and MAX value of semester 1 and semester 2 of all students.
+SELECT MIN(Semester1) as Min1, MAX(Semester1) as Max1,MIN(Semester2) as Min2, MAX(Semester2) as Max2 FROM students;
